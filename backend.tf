031ee777462a9c7f73cb454cfd1e7015c8d1b82b
imran-cd <imran.nadaf@clouddrove.com>
2026-01-21 15:58:20 +0530

    Corrected spacing

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "imran-nadaf-drift-check"
-    key          = "multiple-ec2/terraform.tfstate"
-    region       = "us-east-2"
-    encrypt      = true
+    bucket  = "imran-nadaf-drift-check"
+    key     = "multiple-ec2/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
 }
